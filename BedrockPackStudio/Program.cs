e28267d3d8e833045107e12b207159898464fa43
TRServer Inc. <metoserin8989@gmail.com>
2026-09-05 12:05:27 +0300

    Refactor Program class for Maui application setup

--- BedrockPackStudio/Program.cs
+++ BedrockPackStudio/Program.cs
@@ -1,17 +1,17 @@
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+
 namespace BedrockPackStudio
 {
-    internal static class Program
+    public static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public static MauiApp CreateMauiApp()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>();
+
+            return builder.Build();
         }
     }
 }
