579d5bee9e20bda526e3c454390cf079bf709523
TRServer Inc. <metoserin8989@gmail.com>
2026-09-05 13:50:02 +0300

    Change MainPage initialization to use CreateWindow

--- BedrockPackStudio/App.xaml.cs
+++ BedrockPackStudio/App.xaml.cs
@@ -5,7 +5,11 @@ namespace BedrockPackStudio
         public App()
         {
             InitializeComponent();
-            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
         }
     }
 }
